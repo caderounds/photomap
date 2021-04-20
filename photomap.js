2a71bd354b48127706fc109e3de63f47e24b1665
caderounds <78238647+caderounds@users.noreply.github.com>
2021-04-19 22:23:01 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -14,16 +14,14 @@
  */
 function createPhotoMap () {
   // URL of a Google Sheets spreadsheet output as CSV
-  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSuESp280YE8_PokNYJSK_V3zxGh7ZJlDqMNeliCoz1kt6q2ii7lRovZ-yMlEFY2n6rhUu3vOpnvFGx/pub?gid=0&single=true&output=csv';
-  
-  // create map object with center lat/lon and zoom level
-  var map = L.map('map').setView([30.44, -91.187], 13);
+  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQVgHpoKjIcJzNNnmkN0pu3LDFA0wAlpcjLHYHbtNN4UA2dzyG-X4acT4bXCPIx5ZyUhe3MD8DI2rCs/pub?gid=0&single=true&output=csv';  // create map object with center lat/lon and zoom level
+  var map = L.map('map').setView([30.40503, -91.11565], 12);
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
-  var basemap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-  	maxZoom: 16,
-  	attribution: 'USGS'
-  }).addTo(map);
+ var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
   
   // use Papa Parse (papaparse.com) to get the Google Sheets CSV
   Papa.parse(csvUrl, {
